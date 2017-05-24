e3c549d55f7f52fb4819d9c3be58fcc8e3aba82f
Emoin.Lam <nemoremold@gmail.com>
2017-05-24 20:56:42 +0800

    Control Class added: PagingController

---
+++ Icebox/PagingController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icebox
+{
+
+    class PagingController
+    {
+        private int[] _hashmap;
+        private int _pagingFault;
+        private int _numberOfPages;
+        private int _numberOfInstructionsInEachPage;
+        private int _numberOfInstructions;
+        private int _memorySize;
+
+        public PagingController(int pageCnt, int instrCnt, int size)
+        {
+            _numberOfPages = pageCnt;
+            _numberOfInstructionsInEachPage = instrCnt;
+            _memorySize = size;
+            _numberOfInstructions = _numberOfPages * _numberOfInstructionsInEachPage;
+
+            _hashmap = new int[_numberOfInstructions];
+            _pagingFault = 0;
+        }
+
+        public void operateLeastRecentlyUsedAlgorithm(Page page, ref Memory memory)
+        {
+
+        }
+    }
+}
